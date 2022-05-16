70e2eccb03f8eab2571ad3f5ac6c92c14a5e1148
+ <diazcristina.la@gmail.com>
2022-05-15 18:51:35 -0700

    commit tree 33139da0d92ebbabd70bf5d8bc8ba2fa2d13fd35

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -7,7 +13,9 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+make a function that returns true if any two numbers in the array add up to 10
+
+for every num in the array 
 */
 
 /*
